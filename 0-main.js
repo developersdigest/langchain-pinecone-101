d52ecfc2cb1060799232c367a1249b1b0387ed94
Rob Bowers <rob.bowers@gmail.com>
2023-05-28 15:35:41 -0400

    fix async/await issues in setup flow

--- 0-main.js
+++ 0-main.js
@@ -14,28 +14,28 @@ import { updatePinecone } from "./2-updatePinecone.js";
 import { queryPineconeVectorStoreAndQueryLLM } from "./3-queryPineconeAndQueryGPT.js";
 // 6. Load environment variables
 dotenv.config();
-// 7. Set up DirectoryLoader to load documents from the ./documents directory
-const loader = new DirectoryLoader("./documents", {
-    ".txt": (path) => new TextLoader(path),
-    ".pdf": (path) => new PDFLoader(path),
-});
-const docs = await loader.load();
-// 8. Set up variables for the filename, question, and index settings
+// 7. Set up variables for the filename, question, and index settings
 const question = "Who is mr Gatsby?";
 const indexName = "your-pinecone-index-name";
 const vectorDimension = 1536;
-// 9. Initialize Pinecone client with API key and environment
-const client = new PineconeClient();
-await client.init({
-  apiKey: process.env.PINECONE_API_KEY,
-  environment: process.env.PINECONE_ENVIRONMENT,
-});
-// 10. Run the main async function
+// 8. Run the main async function
 (async () => {
-// 11. Check if Pinecone index exists and create if necessary
+// 9. Initialize Pinecone client with API key and environment
+  const client = new PineconeClient();
+  await client.init({
+    apiKey: process.env.PINECONE_API_KEY,
+    environment: process.env.PINECONE_ENVIRONMENT,
+  });
+// 10. Check if Pinecone index exists and create if necessary
   await createPineconeIndex(client, indexName, vectorDimension);
+// 11. Set up DirectoryLoader to load documents from the ./documents directory
+  const loader = new DirectoryLoader("./documents", {
+    ".txt": (path) => new TextLoader(path),
+    ".pdf": (path) => new PDFLoader(path),
+  });
+  const docs = await loader.load();
 // 12. Update Pinecone vector store with document embeddings
-  // await updatePinecone(client, indexName, docs);
+  await updatePinecone(client, indexName, docs);
 // 13. Query Pinecone vector store and GPT model for an answer
   await queryPineconeVectorStoreAndQueryLLM(client, indexName, question);
 })();
